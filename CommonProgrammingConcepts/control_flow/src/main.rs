8ddb77ec4f44e3610d9307cb8c9310f96cb128c7
hongmaoxiao <769501294@qq.com>
2022-01-31 06:39:28 +0800

    add control flow for

--- CommonProgrammingConcepts/control_flow/src/main.rs
+++ CommonProgrammingConcepts/control_flow/src/main.rs
@@ -38,4 +38,14 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+    let a = [1, 2, 3, 4, 5];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for n in (1..4).rev() {
+        println!("{}!", n);
+    }
 }
