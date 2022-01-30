ca56e0e2aa32484835c734292e8b3c631a70f4a0
hongmaoxiao <769501294@qq.com>
2022-01-31 05:26:59 +0800

    add parameter function

--- CommonProgrammingConcepts/functions/src/main.rs
+++ CommonProgrammingConcepts/functions/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+
+    param_function(5);
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn param_function(x: i32) {
+    println!("The value of x is: {}", x);
 }
