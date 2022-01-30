c8953bc034672d9878ce71b8233bae44c4156160
hongmaoxiao <769501294@qq.com>
2022-01-31 05:33:54 +0800

    add multiple parameter2 function

--- CommonProgrammingConcepts/functions/src/main.rs
+++ CommonProgrammingConcepts/functions/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     another_function();
 
     param_function(5);
+
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function() {
@@ -12,4 +14,8 @@ fn another_function() {
 
 fn param_function(x: i32) {
     println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {}{}", value, unit_label);
 }
