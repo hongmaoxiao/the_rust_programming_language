1012c060e56d9d0010c75c8d6a70428e2c8df46c
hongmaoxiao <769501294@qq.com>
2022-01-31 05:47:57 +0800

    add other functions

--- CommonProgrammingConcepts/functions/src/main.rs
+++ CommonProgrammingConcepts/functions/src/main.rs
@@ -6,6 +6,18 @@ fn main() {
     param_function(5);
 
     print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 6;
+        x
+    };
+    println!("Now y is: {}", y);
+
+    let z = five();
+    println!("The value of z is: {}", z);
+
+    let p = plus_one(5);
+    println!("The value of p is: {}", p);
 }
 
 fn another_function() {
@@ -18,4 +30,12 @@ fn param_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {}{}", value, unit_label);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
