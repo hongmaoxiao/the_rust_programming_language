e6a5405b5868d57485589f776e2eb2abe0318576
hongmaoxiao <769501294@qq.com>
2022-01-31 06:29:46 +0800

    add control flow while

--- CommonProgrammingConcepts/control_flow/src/main.rs
+++ CommonProgrammingConcepts/control_flow/src/main.rs
@@ -17,4 +17,25 @@ fn main() {
         count += 1;
     }
     println!("End count = {}", count);
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
