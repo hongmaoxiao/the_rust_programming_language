76c90e644a23ad15cf88c1763584c816e0c5c4bb
hongmaoxiao <769501294@qq.com>
2022-01-31 05:15:34 +0800

    remove target dir

--- CommonProgrammingConcepts/data_type/target/rls/debug/deps/data_type-055983d7c03d51d8.d
+++
@@ -1,5 +0,0 @@
-/Users/xhm/learn/backend/rust/the_rust_programming_language/CommonProgrammingConcepts/data_type/target/rls/debug/deps/data_type-055983d7c03d51d8.rmeta: src/main.rs
-
-/Users/xhm/learn/backend/rust/the_rust_programming_language/CommonProgrammingConcepts/data_type/target/rls/debug/deps/data_type-055983d7c03d51d8.d: src/main.rs
-
-src/main.rs:
--- CommonProgrammingConcepts/variables/target/rls/debug/deps/variables-a926cec2d0a75833.d
+++
@@ -1,5 +0,0 @@
-/Users/xhm/learn/backend/rust/the_rust_programming_language/CommonProgrammingConcepts/variables/target/rls/debug/deps/variables-a926cec2d0a75833.rmeta: src/main.rs
-
-/Users/xhm/learn/backend/rust/the_rust_programming_language/CommonProgrammingConcepts/variables/target/rls/debug/deps/variables-a926cec2d0a75833.d: src/main.rs
-
-src/main.rs:
