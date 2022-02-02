25a428c16675fca3750aa46ecba9057c8d3d2193
hongmaoxiao <769501294@qq.com>
2022-02-02 21:54:30 +0800

    add struct rectangle struct

--- UsingStructsToStructureRelatedData/rectangle/src/main.rs
+++ UsingStructsToStructureRelatedData/rectangle/src/main.rs
@@ -1,3 +1,8 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -6,6 +11,15 @@ fn main() {
 
     let rect1 = (30, 50);
     println!("The area of the rectangle is {} square pixels.", area_tuple(rect1));
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", area_struct(&rect2));
+
+    println!("{}-{}", rect2.width, rect2.height);
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -14,4 +28,8 @@ fn area(width: u32, height: u32) -> u32 {
 
 fn area_tuple(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+fn area_struct(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
 }
