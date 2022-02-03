b56a96ffb38e2b6c7bf0bccbe863148cf30c1667
hongmaoxiao <769501294@qq.com>
2022-02-03 19:50:29 +0800

    add rectangle associated functions

--- UsingStructsToStructureRelatedData/methods/src/main.rs
+++ UsingStructsToStructureRelatedData/methods/src/main.rs
@@ -5,8 +5,23 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -16,5 +31,26 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let rect4 = Rectangle::square(30);
+
     println!("The area of the rectangle is {} square pixels.", rect1.area());
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("The area of the rect4 is {} square pixels.", rect4.area());
 }
