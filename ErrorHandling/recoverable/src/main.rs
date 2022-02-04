c240073286b8e2ca29d38456ef4ced6ed597ac88
hongmaoxiao <769501294@qq.com>
2022-02-05 00:59:58 +0800

    Matching on Different Errors

---
+++ ErrorHandling/recoverable/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+fn main() {
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e)
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+}
