67b55d58d368e162b5b192a53675321756931007
hongmaoxiao <769501294@qq.com>
2022-02-04 08:43:55 +0800

    paths in module tree

--- ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
+++ ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -12,4 +12,23 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
