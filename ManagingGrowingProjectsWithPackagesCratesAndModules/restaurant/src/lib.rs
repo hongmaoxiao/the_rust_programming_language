14dae1e2ff5fb92f0f7539169d3f6eb0b58553aa
hongmaoxiao <769501294@qq.com>
2022-02-04 09:21:35 +0800

    paths into scope with the use keyword

--- ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
+++ ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
@@ -14,6 +14,9 @@ mod front_of_house {
     }
 }
 
+
+use crate::front_of_house::hosting;
+
 fn serve_order() {}
 
 mod back_of_house {
@@ -50,6 +53,10 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
