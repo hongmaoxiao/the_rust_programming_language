b7cd6bf6d757e4d3d890977738b488ca1672895b
hongmaoxiao <769501294@qq.com>
2022-02-04 08:59:07 +0800

    make enum public

--- ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
+++ ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
@@ -37,6 +37,11 @@ mod back_of_house {
             }
         }
     }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 pub fn eat_at_restaurant() {
@@ -55,4 +60,7 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
