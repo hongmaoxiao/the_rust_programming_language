6fddf8d5d58dad6b87f6e5b1543135cb0cedd6a3
hongmaoxiao <769501294@qq.com>
2022-02-04 08:57:40 +0800

    make structs public

--- ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
+++ ManagingGrowingProjectsWithPackagesCratesAndModules/restaurant/src/lib.rs
@@ -23,6 +23,20 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
 
 pub fn eat_at_restaurant() {
@@ -31,4 +45,14 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
