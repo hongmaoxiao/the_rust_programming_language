3ec461b0435f86bb81eacb7d48dc246ddfafaea0
hongmaoxiao <769501294@qq.com>
2022-02-06 20:45:01 +0800

    reading a file

--- BuildingACommandLineProgram/minigrep/src/main.rs
+++ BuildingACommandLineProgram/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +9,7 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
