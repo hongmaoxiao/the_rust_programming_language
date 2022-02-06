af834b533f39e04351cd0d5b8683c527a86636a8
hongmaoxiao <769501294@qq.com>
2022-02-06 15:20:13 +0800

    add test comman params

--- WritingAutomatedTests/adder/src/lib.rs
+++ WritingAutomatedTests/adder/src/lib.rs
@@ -69,6 +69,35 @@ mod tests {
             Err(String::from("two plus two does not equal four"))
         }
     }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    // #[test]
+    // fn this_test_will_fail() {
+    //     let value = prints_and_returns_10(8);
+    //     assert_eq!(5, value);
+    // }
+
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    #[ignore]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
 }
 
 #[derive(Debug)]
@@ -107,4 +136,9 @@ impl Guess {
             value
         }
     }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
 }
