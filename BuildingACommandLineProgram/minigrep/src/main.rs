b7fb77eaaab5aa7fea62a778b7e4d2524f295bd4
hongmaoxiao <769501294@qq.com>
2022-02-07 10:16:04 +0800

    complete command line tool

--- BuildingACommandLineProgram/minigrep/src/main.rs
+++ BuildingACommandLineProgram/minigrep/src/main.rs
@@ -1,15 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|error| {
+        eprintln!("Problem parsing arguments: {}!", error);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+        process::exit(-1);
+    });
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}!", e);
+
+        process::exit(-1);
+    }
 }
