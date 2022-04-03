ea2318e64a59af31ee53850ee543b6f4f14f3635
hongmaoxiao <769501294@qq.com>
2022-04-03 17:00:12 +0800

    workout with closure

--- FunctionalLanguageFeaturesIteratorsAndClosures/main.rs
+++ FunctionalLanguageFeaturesIteratorsAndClosures/main.rs
@@ -1,23 +1,21 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-  println!("calculating slowly...");
-  thread::sleep(Duration::from_secs(2));
-  intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
-  let expensive_result = simulated_expensive_calculation(intensity);
+  let expensive_closure = |num| {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    num
+  };
 
   if intensity < 25 {
-    println!("Today, do {} pushups!", expensive_result);
-    println!("Next, do {} situps!", expensive_result);
+    println!("Today, do {} pushups!", expensive_closure(intensity));
+    println!("Next, do {} situps!", expensive_closure(intensity));
   } else {
     if random_number == 3 {
       println!("Take a break today! Remember to stay hydrated!");
     } else {
-      println!("Today, run for {} minutes!", expensive_result);
+      println!("Today, run for {} minutes!", expensive_closure(intensity));
     }
   }
 }
