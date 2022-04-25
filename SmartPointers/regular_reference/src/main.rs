acdf6f460951d304d45f70e7f0d2992ba1dc0cef
hongmaoxiao <769501294@qq.com>
2022-04-25 23:23:15 +0800

    add define own smart pointer

---
+++ SmartPointers/regular_reference/src/main.rs
@@ -0,0 +1,28 @@
+use std::ops::Deref;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    // let y = &x;
+    // let y = Box::new(x);
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+
