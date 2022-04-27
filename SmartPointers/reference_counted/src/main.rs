92b9115343d7336769cb3a6f242ef9df4873a933
hongmaoxiao <769501294@qq.com>
2022-04-27 16:06:55 +0800

    add Using Rc<T> to Share Data, the right code

--- SmartPointers/reference_counted/src/main.rs
+++ SmartPointers/reference_counted/src/main.rs
@@ -1,13 +1,14 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
 
