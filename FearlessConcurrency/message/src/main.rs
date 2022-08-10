da289bf78be924c68a5cdfcd855712344b8ca842
hongmaoxiao <769501294@qq.com>
2022-08-11 00:28:47 +0800

    add multiple values multiple receivers

--- FearlessConcurrency/message/src/main.rs
+++ FearlessConcurrency/message/src/main.rs
@@ -5,12 +5,28 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
             String::from("from"),
             String::from("the"),
             String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("message"),
+            String::from("for"),
+            String::from("you"),
         ];
 
         for val in vals {
